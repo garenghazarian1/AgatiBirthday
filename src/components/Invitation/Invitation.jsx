d1837572441bbf7e507db171e93933ffe19a88b3
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2025-03-08 01:44:43 +0100

    edit the invitation

--- src/components/Invitation/Invitation.jsx
+++ src/components/Invitation/Invitation.jsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./Invitation.module.css";
 import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
 
@@ -18,53 +19,64 @@ export default function Invitation() {
     return <p className={styles.loading}>Loading invitation...</p>;
   }
 
-  const handleRSVP = () => {
-    alert(t("rsvpMessage"));
-  };
-
   return (
     <>
-      <LanguageSwitcher />
+      <div className={styles.celebrationBackground}>
+        <LanguageSwitcher />
 
-      <div className={styles.topShadow}></div>
-      {/* <div className={styles.bottomShadow}></div> */}
-      <div className={styles.topShadow}></div>
-      <div className={`${styles.card} ${styles.shimmerBorder}`}>
-        <h1 className={styles.title}>{t("invitationTitle")}</h1>
-        <div className={styles.textContainer}>
-          <p className={styles.subtitle}>{t("invitationSubtitle")}</p>
-          <p className={styles.details}>{t("invitationDetails")}</p>
-          <p className={styles.details}>{t("invitationLocation")}</p>
+        <div className={`${styles.card} ${styles.shimmerBorder}`}>
+          <Image
+            src="/001.png"
+            alt="kross"
+            width={500}
+            height={500}
+            className={styles.invitationImage}
+            priority
+          />
+          <div className={styles.decorativeContainer}>
+            <div className={styles.decorativeShape}>
+              <Image
+                src="/ani.jpg"
+                alt="Ani"
+                width={500}
+                height={500}
+                className={styles.person}
+                priority
+              />
+            </div>
 
-          {/* Google Maps Link */}
-          <Link
-            href="https://www.google.com/maps?q=576R+PGF,+Vagharshapat,+Armenia"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.googleMapsLink}
-            aria-label="View location on Google Maps"
-          >
-            {t("googleMapsLink")}
-          </Link>
+            <div className={styles.decorativeShape}>
+              <Image
+                src="/agati.jpg"
+                alt="Agati"
+                width={500}
+                height={500}
+                className={styles.person}
+                priority
+              />
+            </div>
+          </div>
+
+          <h1 className={styles.title}>{t("invitationTitle")}</h1>
+          <div className={styles.textContainer}>
+            <p className={styles.subtitle}>{t("invitationSubtitle")}</p>
+            <p className={styles.details}>{t("invitationMessage")}</p>
+
+            <p className={styles.details}>{t("invitationDetails")}</p>
+            <p className={styles.details}>{t("invitationLocation")}</p>
+
+            {/* Google Maps Link */}
+            <Link
+              href="https://www.google.com/maps?q=576R+PGF,+Vagharshapat,+Armenia"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.googleMapsLink}
+              aria-label="View location on Google Maps"
+            >
+              {t("googleMapsLink")}
+            </Link>
+          </div>
         </div>
-        {/* Embedded Google Map */}
-        {/* <div className={styles.mapContainer}>
-        <iframe
-          title="Etchmiadzin Cathedral Location"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3048.2186482758763!2d44.291!3d40.161861!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406abcfce370c197%3A0x69f406c27837b640!2sEchmiadzin%20Cathedral!5e0!3m2!1sen!2sam!4v1712000000000"
-          width="100px"
-          height="100px"
-          className={styles.mapIframe}
-          allowFullScreen
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </div> */}
-
-        {/* RSVP Button */}
-        {/* <button className={styles.rsvpButton} onClick={handleRSVP}>
-        {t("rsvpButton")}
-      </button> */}
       </div>
     </>
   );
